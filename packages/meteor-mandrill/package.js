8cee8f283fe7e3934de43f6c3980c437d005a017
ABattut <arthur.battut@gmail.com>
2014-05-26 21:13:21 +0200

    finish form verification

--- packages/meteor-mandrill/package.js
+++
@@ -1,7 +0,0 @@
-Package.describe({
-  summary: "send email via Mandrill"
-});
-
-Package.on_use(function(api) {
-  api.add_files('mandrill.js', 'server');
-});
--- udo.js
+++ udo.js
@@ -1,25 +1,52 @@
 if (Meteor.isClient) {
-
+	Template.hello.events({
+		'click #btn-submit': function(){
+			var to = $('#email').val(),
+				htmlText = $('#message').val(),
+				from = $('#name').val(),
+				goodArray = [false, false];
+			
+			if(to=="")
+				$('#email').addClass('error');
+			else{
+				$('#email').removeClass('error');
+				goodArray[0]=true;
+			}
+			if(htmlText=="")
+				$('#message').addClass('error');
+			else{
+				$('#message').removeClass('error');
+				goodArray[1]=true;
+			}
+			
+			if(goodArray[0] && goodArray[1]){
+				var subject = "J'ai un message pour vous";
+				if(from!=""){
+					subject+= " de "+from;
+					message+= "\n \n"+from;
+				}
+				Meteor.call('sendMail', to, subject, message);
+			}
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    Meteor.startup(function(){
-      Meteor.Mandrill.config({
-        username: "USERNAME",
-        key: "KEY"
-      });
-    });
+	Meteor.startup(function(){
+		Meteor.Mandrill.config({
+			username: "USERNAME",
+			key: "KEY"
+		});
+	});
 
-    Meteor.methods({
-      sendMail: function(to, subject, htmlText){
-        Meteor.Mandrill.send({
-          to: to,
-          from: "MAIL",
-          subject: subject,
-          html: htmlText
-        });
-      }
-    });
-  });
+	Meteor.methods({
+		sendMail: function(to, subject, htmlText){
+			Meteor.Mandrill.send({
+				to: to,
+				from: "MAIL",
+				subject: subject,
+				html: htmlText
+			});
+		}
+	});
 }
